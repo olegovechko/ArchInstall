cea96b0c11562a1643eb64347b221312035d99e3
olegovechko <oleg.subzero@gmail.com>
2024-03-24 14:56:13 +0200

    Update download-server.sh

--- download-server.sh
+++ download-server.sh
@@ -2,5 +2,5 @@
 #https://drive.google.com/file/d/11kyzmcBYfyVFVYmedeUACvEms3Nd_mcy/view?usp=drive_link
 fileid="11kyzmcBYfyVFVYmedeUACvEms3Nd_mcy"
 filename="LinuxServer.zip"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
