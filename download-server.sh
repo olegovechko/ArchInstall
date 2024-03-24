fc2e7e67d14be488d019161bb0ba20215ca97b3a
olegovechko <oleg.subzero@gmail.com>
2024-03-24 15:05:29 +0200

    Update download-server.sh

--- download-server.sh
+++ download-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #https://drive.google.com/file/d/11kyzmcBYfyVFVYmedeUACvEms3Nd_mcy/view?usp=drive_link
-fileid="11kyzmcBYfyVFVYmedeUACvEms3Nd_mcy"
-filename="LinuxServer.zip"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+ggID='11kyzmcBYfyVFVYmedeUACvEms3Nd_mcy'  
+ggURL='https://drive.google.com/uc?export=download'  
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"  
